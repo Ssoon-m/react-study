d2a93af6752e341a7acc6f04bfe582502d1888dc
순민 <tkksm2@gmail.com>
2022-03-13 18:47:18 +0900

    useContext를 이용한 상태관리

---
+++ src/store/favorites-context.js
@@ -0,0 +1,47 @@
+import { createContext, useState } from 'react';
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => { },
+  removeFavorite: (meetupId) => { },
+  itemIsFavorite: (meetupId) => { }
+});
+
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    })
+  }
+
+  // 응답을 반환한다.
+  // 스테이트를 업데이트 할 필요가 없다.
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  )
+}
+
+export default FavoritesContext;
